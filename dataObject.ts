22d9b4a943fdb319eb9796e9f05df0f761e1f5a7
ifralou <ilya.fralou@icloud.com>
2023-05-20 13:12:42 +0200

    dataObject added.

---
+++ dataObject.ts
@@ -0,0 +1,71 @@
+type RoomLayout = "u-shape" | "standard" //something else
+
+type NoiseLevel = "low" | "standard" | "high";
+
+type RoomFeatures =
+    "conditioning" | "projector" | "white" | "printer" | "sound" | "wifi" | "phone" | "accessiblility" | "refreshment"
+
+type RoomSlot = {
+    from: Date,
+    to: Date
+}
+
+type RoomFull = {
+    id: number,
+    "buildingId": number,
+    "name": string,
+    "description": string,
+    img: string,
+    occupiedNow: boolean,
+    capacity: number,
+    reservedSlots: [RoomSlot],
+    roomFeatures: [RoomFeatures],
+    layout: RoomLayout,
+    noiseLevel: NoiseLevel
+};
+
+type RoomForDashBoard = {
+    id: number,
+    "buildingId": number,
+    "name": string,
+    "description": string,
+    img: string,
+    occupiedNow: boolean,
+}
+
+type getRoomsForDashBoardResponse = {
+    rooms: [RoomForDashBoard],
+    currentPageNumber: number
+    totalPages: number
+};
+
+interface RoomManagmentInterface {
+    getAllRoomsForDashboard(currentPageNumber: number): getRoomsForDashBoardResponse;
+    getRoomFull(buildingId: number, roomId: number): RoomFull;
+}
+
+type UserReservation = {
+    reservationId: number,
+    roomName: string,
+    from: Date,
+    to: Date
+}
+
+interface ReservationManagmentInterface {
+    reserveRoom(buildingId: number, roomId: number, userId: number, from: number, to: number): Boolean
+    getReservedRoomsByUserId(userId: number): [UserReservation]
+    deleteRoomReservation(reservationId): boolean
+}
+
+type Building = {
+    id: number,
+    name: string
+}
+
+
+interface AdminRoomManagment {
+    getAllBuildings(): [ Building ]
+    deleteBuilding(buildingId: number): boolean
+    addBuilding(): boolean
+    newEquipmentStateForTheRoom(buildingId: number, roomId: number, newRoomFeatures: [RoomFeatures]): [RoomFeatures]
+}

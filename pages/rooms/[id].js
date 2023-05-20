01b3e817ea568fe6d7337c4c4f9dfec657120b3b
ifralou <ilya.fralou@icloud.com>
2023-05-20 18:51:08 +0200

    Room page changes to clientSide component

--- pages/rooms/[id].js
+++ pages/rooms/[id].js
@@ -1,25 +1,26 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SiteWrapper from "@/components/SiteWrapper/SiteWrapper";
 import {roomByIdInfoFetcher} from "@/connectors/fetchers";
-import RoomEmpty from "@/components/RoomCardDetailed/RoomEmpty";
 import RoomCardDetailed from "@/components/RoomCardDetailed/RoomCardDetailed";
+import {useRouter} from "next/router";
+import Loader from "@/components/Loader/Loader";
 
 //localhost:3000/rooms/1
 
-export default function RoomOverview({room}) {
+export default function RoomOverview() {
+    const router = useRouter();
+    const [room, setRoom] = useState(null);
+
+    useEffect(() => {
+       roomByIdInfoFetcher(router.query.id)
+           .then(res => setRoom(res))
+    },[]);
+
     return (
         <SiteWrapper>
             {
-                room ? <RoomCardDetailed room={room}/> : <RoomEmpty/>
+                room ? <RoomCardDetailed room={room}/> : <Loader/>
             }
         </SiteWrapper>
     );
-}
-
-export async function getServerSideProps(context) {
-    return {
-        props: {
-            room: await roomByIdInfoFetcher(context.query.id)
-        }
-    }
 }
